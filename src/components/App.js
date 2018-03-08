fd681e68902b103961a1498fd5ff6a6eaf4dec5a
Alec Jandorek <ajandorek@gmail.com>
2018-03-08 15:24:51 -0600

    Refactored fetchUser action creator to use async/await syntax

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import { connect } from 'react-redux';
-import * as actions from '../actions';
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
-import Header from './Header';
-const Dashboard = () => <h2>Dashboard</h2>
-const SurveyNew = () => <h2>SurveyNew</h2>
-const Landing = () => <h2>Landing</h2>
-
+import Header from "./Header";
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
   componentDidMount() {
@@ -16,18 +15,18 @@ class App extends Component {
 
   render() {
     return (
-      <div className='container'>
+      <div className="container">
         <BrowserRouter>
           <div>
             <Header />
-            <Route exact path='/' component={Landing} />
-            <Route exact path='/surveys' component={Dashboard} />
-            <Route path='/surveys/new' component={SurveyNew} />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
           </div>
         </BrowserRouter>
       </div>
     );
   }
-};
+}
 
 export default connect(null, actions)(App);
