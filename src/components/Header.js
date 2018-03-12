820eb6c832179ba972d59fbdd8d3c240945c3719
Alec Jandorek <ajandorek@gmail.com>
2018-03-12 16:06:03 -0500

    Created component for handling user credit payments

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,15 +26,10 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <Link
-            to={this.props.auth ? '/surveys' : '/'}
-            className="left brand-logo"
-          >
+          <Link to={this.props.auth ? '/surveys' : '/'} className="left brand-logo">
             Emaily
           </Link>
-          <ul className="right">
-            <li>{this.renderContent()}</li>
-          </ul>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import 'materialize-css/dist/css/materialize.min.css';
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
@@ -16,3 +16,6 @@ ReactDOM.render(
   </Provider>,
   document.querySelector('#root')
 );
+
+console.log('STRIPE KEY IS', process.env.REACT_APP_STRIPE_KEY);
+console.log(process.env.NODE_ENV);
